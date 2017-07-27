cf9892f9c40308e7843a2e46b2a24999eb64d934
김창회 <kopr3963@naver.com>
2017-07-27 21:44:28 +0900

    capture.py

--- Capture.py
+++ Capture.py
@@ -2,6 +2,36 @@
 #_*_coding: utf-8-*-
 
 
+import cv2
+import numpy as numpy
 
 
-####
+
+
+def settings():
+	try:
+		print '카메라 구동';
+		cap = cv2.VideoCapture(0);
+	except:
+		print '카메라 구동 실패';
+		return;
+
+	cap.set(3, 480);
+	cap.set(4, 320);
+
+	while True:
+		ret,frame = cap.read();
+
+		if not ref:
+			print('ret is error');
+			break;
+
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY);
+		cv2.imshow('video',gray);
+
+		k = cv2.waitKey(1) & 0xFF;
+		if k == 27:
+			break;
+		cap.release();
+		cv2.destroytAllWindows();
+settings();
