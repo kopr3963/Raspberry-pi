d7a954a31a155337312ae427449c616661be498d
김창회 <kopr3963@naver.com>
2017-07-28 16:56:35 +0900

    들여쓰기 이상.

--- Capture2.py
+++ Capture2.py
@@ -14,20 +14,19 @@ while(True):
 
   	lines = cv2.HoughLines(edges, 1, np.pi/180,140);
 
-  	for line  in lines:
-    	r, theta = line[0]
-    	a = np.cos(theta)
-    	b = np.sin(theta)
-	    x0 = a*r
-	    y0 = b*r
-	    x1 = int(x0 + 1000*(-b))
-	    y1 = int(y0 + 1000*a)
-	    x2 = int(x0 - 1000*(-b))
-	    y2 = int(y0 - 1000*a)
-
-	    cv2.line(ret, (x1, y1), (x2, y2), (0,255,0),1)
-
-
+  	for line in lines:
+  		r,theta = line[0];
+  		a = np.cos(theta);
+  		b = np.sin(theta);
+  		x0 = a*r;
+  		y0 = b*r;
+  		x1 = int(x0 + 1000* (-b));
+  		y1 = int(y0 + 1000*a);
+  		x2 = int(x0 - 1000*(-b));
+  		y2 = int(y0 - 1000*a));
+		
+		cv2.line(ret, (x1, y1), (x2, y2), (0,255,0,),1);
+		
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 	 break;
 
